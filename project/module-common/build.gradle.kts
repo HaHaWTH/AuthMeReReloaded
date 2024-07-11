34e52d7ecee304164f96f701867f2c5091c8f7b2
MC~蛟龙 <1610105206@qq.com>
2024-07-11 21:31:56 +0800

    Fix build work 2

--- project/module-common/build.gradle.kts
+++ project/module-common/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
-    compileOnly(project(":project:module-configuration"))
     compileOnly(project(":project:module-util"))
     compileOnly(project(":project:module-logger"))
     compileOnly(project(":project:module-security"))
